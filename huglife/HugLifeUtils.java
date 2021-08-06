2186da31b84525e46c0f3cdde83fec8e129e3e12
Po <pmarinova@gmail.com>
2021-08-06 11:52:44 +0300

    Refactored HugLifeUtils.java.

--- huglife/HugLifeUtils.java
+++ huglife/HugLifeUtils.java
@@ -6,33 +6,27 @@ import java.util.Random;
 /** Utilities for lab 5
  *  @author Josh Hug
  */
-public class HugLifeUtils { // TODO make me interface
-    private static Random r = null;
+public interface HugLifeUtils {
+    Random r = new Random();
 
     /** Returns a random number uniformly between 0 and 1 */
-    public static double random() {
-        if (r == null)
-            r = new Random();
-
+    static double random() {
         return r.nextDouble();
     }
 
     /** Returns a random number uniformly between min and max inclusive
         Stolen from: http://stackoverflow.com/questions/363681 */
-    public static int randomInt(int min, int max) {
-        if (r == null)
-            r = new Random();
-
+    static int randomInt(int min, int max) {
         return r.nextInt((max - min) + 1) + min;
     }
 
     /** Returns a random number uniformly between 0 and max */
-    public static int randomInt(int max) {
+    static int randomInt(int max) {
         return randomInt(0, max);
     }
 
     /** Returns a random number uniformly between 0 and max */
-    public static Direction randomEntry(List<Direction> directions) {
+    static Direction randomEntry(List<Direction> directions) {
         var dirIndex = randomInt(directions.size() - 1);
         return directions.get(dirIndex);
     }
